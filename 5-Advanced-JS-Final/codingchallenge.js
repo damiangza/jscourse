d16354067bd284e48db36bb8e398edbbef8a7a9d
damiangza <damiangbbs@gmail.com>
2018-05-07 21:40:37 +0200

    just a test

--- 5-Advanced-JS-Final/codingchallenge.js
+++ 5-Advanced-JS-Final/codingchallenge.js
@@ -86,6 +86,8 @@ console.log(checkAnswer(questionPrompt));
 /*
 6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
 
+
+
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
